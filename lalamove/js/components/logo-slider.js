b3c6949a5e922894bdc08d207e7991afcec7e5c8
Cty0305 <mickeychou375@gmail.com>
2024-09-08 15:15:52 +0800

    update modules

---
+++ lalamove/js/components/logo-slider.js
@@ -0,0 +1,20 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   var swiper = new Swiper(".swiper-container", {
+//     loop: true,
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     autoplay: {
+//       delay: 5000,
+//       disableOnInteraction: false,
+//     },
+//     slidesPerView: 1,
+//     spaceBetween: 10,
+//   });
+// });
+// export default initSwiper;
