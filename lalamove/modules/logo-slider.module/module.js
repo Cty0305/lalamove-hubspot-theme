141a997cea5d2ed8188604e5a236950f2393b346
Cty0305 <mickeychou375@gmail.com>
2024-09-17 15:28:52 +0800

    modules update

--- lalamove/modules/logo-slider.module/module.js
+++ lalamove/modules/logo-slider.module/module.js
@@ -1,19 +1,19 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var swiper = new Swiper(".swiper-container", {
-    loop: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    slidesPerView: 6,
-    spaceBetween: 10,
-  });
-});
+// document.addEventListener("DOMContentLoaded", function () {
+//   var swiper = new Swiper(".swiper-container", {
+//     loop: true,
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     autoplay: {
+//       delay: 5000,
+//       disableOnInteraction: false,
+//     },
+//     slidesPerView: 6,
+//     spaceBetween: 10,
+//   });
+// });
